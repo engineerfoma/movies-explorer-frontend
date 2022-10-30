f5b07955ae5ba1cbf694116a389b1b4729760e2c
Nikolay Fomenko <fomen_94@bk.ru>
2022-10-31 00:15:13 +0300

    feach: начать делать routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,23 +1,44 @@
-import './App.css';
+import './App.scss';
+import { currentUserContext } from '../../context/CurrentUserContext';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Header from '../Header/Header';
+import ProtectedRoute from '../ProtectedRoute';
+import Login from '../Login/Login';
+import Register from '../Register/Register';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <currentUserContext.Provider>
+      <div className="page">
+        <Header
+        />
+        <Switch>
+          <ProtectedRoute
+            exact
+            path="/"
+          />
+          <Route path="/signin">
+            <Login
+              onLogin={onLogin}
+            />
+          </Route>
+          <Route path="/signup">
+            <Register
+            />
+          </Route>
+          <Route>
+            {loggedIn ? (
+              <Redirect to="/" />
+            ) : (
+              <Redirect to="/signin" />
+            )}
+          </Route>
 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        </Switch>
+        <Footer />
+      </div>
+    </currentUserContext.Provider >
   );
 }
 
